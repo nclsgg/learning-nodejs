9d789f617552eb81156145ef573dd3d3e7210459
nclsgg <nicolasguadagno@gmail.com>
2021-10-14 14:26:47 -0300

    Table Creation

---
+++ src/database/tables.js
@@ -0,0 +1,24 @@
+class Tables {
+  init(connection) {
+    this.connection = connection
+
+    this.createAttendances()
+
+    console.log("Tabelas foram chamadas")
+  }
+
+  createAttendances() {
+    const sql =
+      "CREATE TABLE IF NOT EXISTS Attendances (id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, status varchar(20) NOT NULL, observations text, PRIMARY KEY(id))"
+
+    this.connection.query(sql, (error) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log("Tabela Atendimentos criada com sucesso")
+      }
+    })
+  }
+}
+
+module.exports = new Tables()
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const customExpress = require("./config/customExpress")
+const connection = require("./database/connection")
+const Tables = require("./database/tables")
+
+connection.connect((error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log("Conexão bem sucedida")
+
+    Tables.init(connection)
+    const app = customExpress()
+
+    app.listen(3000, () => console.log("servidor rodando na porta 3000"))
+  }
+})
