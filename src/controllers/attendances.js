1c6d30753f76a317e49225675a1bed01292496cf
nclsgg <nicolasguadagno@gmail.com>
2021-10-14 17:24:26 -0300

    Entry validations

--- src/controllers/attendances.js
+++ src/controllers/attendances.js
@@ -8,7 +8,6 @@ module.exports = (app) => {
   app.post("/atendimentos", (req, res) => {
     const attendance = req.body
 
-    Attendance.add(attendance)
-    res.send("Post atendimento")
+    Attendance.add(attendance, res)
   })
 }
--- src/models/attendances.js
+++ src/models/attendances.js
@@ -2,22 +2,47 @@ const moment = require("moment")
 const conexao = require("../database/connection")
 
 class Attendance {
-  add(attendance) {
+  add(attendance, res) {
     const createdDate = moment().format("YYYY-MM-DD HH:MM:SS")
+
     const date = moment(attendance.date, "DD/MM/YYYY").format(
       "YYYY-MM-DD HH:MM:SS"
     )
-    const datedAttendance = { ...attendance, createdDate, date }
 
-    const sql = "INSERT INTO Attendances SET ?"
+    const isDateValid = moment(date).isSameOrAfter(createdDate)
+    const isClientValid = attendance.client.length >= 5
 
-    conexao.query(sql, datedAttendance, (error, results) => {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log(results)
-      }
-    })
+    const validations = [
+      {
+        name: date,
+        valid: isDateValid,
+        message: "Não é possível marcar um agendamento para o passado.",
+      },
+      {
+        name: "client",
+        valid: isClientValid,
+        message: "O nome do cliente deve ter pelo menos 5 caracteres.",
+      },
+    ]
+
+    const errors = validations.filter((camp) => !camp.valid)
+    const errorsExists = errors.length
+
+    if (errorsExists) {
+      res.status(400).json(errors)
+    } else {
+      const datedAttendance = { ...attendance, createdDate, date }
+
+      const sql = "INSERT INTO Attendances SET ?"
+
+      conexao.query(sql, datedAttendance, (error, results) => {
+        if (error) {
+          res.status(400).json(error)
+        } else {
+          res.status(201).json(results)
+        }
+      })
+    }
   }
 }
 
